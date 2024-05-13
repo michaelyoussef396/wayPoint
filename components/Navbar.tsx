89165a920657901b58c6a28a77cf33f504b7df57
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-14 01:52:46 +1000

    create constants data

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image' // Import the Image component from the 'next/image' package
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <nav className='border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5'>
+            <Link href="/" >
+                <Image src="/logo.jpg" alt='logo' width={74} height={29}/>
+            </Link>
+
+           <ul className="hidden h-full gap-12 lg:flex">
+                
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
