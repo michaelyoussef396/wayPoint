f18cd29dfe92f518f0e2e09c8bd22d38d7dae2d3
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-14 01:57:00 +1000

    created navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image' // Import the Image component from the 'next/image' package
+import { NAV_LINKS } from '@/constants'
+
 
 const Navbar = () => {
     return (
@@ -9,7 +11,11 @@ const Navbar = () => {
             </Link>
 
            <ul className="hidden h-full gap-12 lg:flex">
-                
+                {NAV_LINKS.map((link) => (
+                    <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+                        {link.label}
+                    </Link>
+                ))}
             </ul>
         </nav>
     )
